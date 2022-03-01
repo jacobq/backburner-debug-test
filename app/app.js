c546a0c0b747fabc0116658417a67c74d0ab9980
Jacob Quant <jacobq@gmail.com>
2022-03-01 11:44:24 -0600

    feat: use _backburner import

--- app/app.js
+++ app/app.js
@@ -3,6 +3,14 @@ import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'backburner-debug-test/config/environment';
 
+// This doesn't work: Uncaught TypeError: Cannot set properties of undefined (setting 'debug')
+//import { backburner } from '@ember/runloop';
+//backburner.debug = true;
+
+import { _backburner } from '@ember/runloop';
+_backburner.DEBUG = true;
+console.log(`Set _backburner.DEBUG = ${_backburner.DEBUG}`);
+
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
   podModulePrefix = config.podModulePrefix;
---
+++ app/controllers/application.js
@@ -0,0 +1,18 @@
+import { tracked } from '@glimmer/tracking';
+import Controller from '@ember/controller';
+import { action } from '@ember/object';
+import { _backburner } from '@ember/runloop';
+
+export default class ApplicationController extends Controller {
+  dummy = console.log(`Application controller about to initialize isBackburnerDebuggingEnabled to ${_backburner.DEBUG}`);
+  @tracked isBackburnerDebuggingEnabled = _backburner.DEBUG;
+
+  @action toggle() {
+    console.log('About to toggle... Old state was:', {
+      "_backburner.DEBUG": _backburner.DEBUG,
+      isBackburnerDebuggingEnabled: this.isBackburnerDebuggingEnabled,
+    });
+    _backburner.DEBUG = !_backburner.DEBUG;
+    this.isBackburnerDebuggingEnabled = _backburner.DEBUG;
+  }
+}
