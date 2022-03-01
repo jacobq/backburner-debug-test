006a5c76b1ed194768b31ee2fa58a122d66ff06d
Jacob Quant <jacobq@gmail.com>
2022-03-01 11:46:37 -0600

    style: let prettier do its thing

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -4,12 +4,14 @@ import { action } from '@ember/object';
 import { _backburner } from '@ember/runloop';
 
 export default class ApplicationController extends Controller {
-  dummy = console.log(`Application controller about to initialize isBackburnerDebuggingEnabled to ${_backburner.DEBUG}`);
+  dummy = console.log(
+    `Application controller about to initialize isBackburnerDebuggingEnabled to ${_backburner.DEBUG}`
+  );
   @tracked isBackburnerDebuggingEnabled = _backburner.DEBUG;
 
   @action toggle() {
     console.log('About to toggle... Old state was:', {
-      "_backburner.DEBUG": _backburner.DEBUG,
+      '_backburner.DEBUG': _backburner.DEBUG,
       isBackburnerDebuggingEnabled: this.isBackburnerDebuggingEnabled,
     });
     _backburner.DEBUG = !_backburner.DEBUG;
